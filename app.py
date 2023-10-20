d989bea7635e068ef0e3abec9aab63e1a7e5b5ff
Srujan rai <raisrujan@gmail.com>
2023-10-20 08:23:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,30 +7,26 @@ from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 
-# Load the deepfake model
 model = keras.models.load_model('deepfake_model.h5')
 
-# Set the image dimensions
 image_height = 640
 image_width = 480
 
-# Define a function to process image uploads
 def process_image(file):
     filename = secure_filename(file.filename)
     file_path = os.path.join('uploads', filename)
-    file.save(file_path)  # Save the uploaded file
+    file.save(file_path) 
     img = image.load_img(file_path, target_size=(image_height, image_width))
     img = image.img_to_array(img)
     img = np.expand_dims(img, axis=0)
     img = img / 255.0
     return img
 
-# Define the home route
 @app.route('/')
 def home():
     return render_template('upload.html', prediction=None)
 
-# Define the prediction route
+
 @app.route('/predict', methods=['POST'])
 def predict():
     if 'file' not in request.files:
@@ -51,6 +47,5 @@ def predict():
         return render_template('upload.html', prediction=predicted_class, image_path=f'uploads/{file.filename}')
 
 if __name__ == '__main__':
-    # Create the 'uploads' directory if it doesn't exist
     os.makedirs('uploads', exist_ok=True)
     app.run(debug=True)
